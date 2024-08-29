f2ed41394c4bf589f778629a28592b2a2c674e32
antonio <antonyejoao@gmail.com>
2024-08-29 17:19:12 -0300

    resolvendo requisição

--- Aulas/Aulas-Presenciais/validacao.js
+++ Aulas/Aulas-Presenciais/validacao.js
@@ -5,15 +5,35 @@
 
 function validarNome() {
     
-    let nome = document.getElementById("nameInput").value
-    let email = document.getElementsByName("emailInput").value
-
-    if(nome.length == 0) {
+    let elemento = document.getElementById("name").value
+    let valorElemento = elemento.value
+    
+    if(elemento.length == 0) {
+        alert("digite algo no nome")
+        elemento.Style.border = 'red solid 3px'
         
+    } else {
+        alert("O nome digitado foi: " + elemento)
+        elemento.Style.border = 'green solid 3px'
     }
 }
 
+function validarEmail() {
+    let email = document.getElementById("emailInput").value
+    let valorEmail  = email.value
+
+    if(valorEmail.length == 0) {
+        alert("digite algo no email")
+        elemento.Style.border = 'red solid 3px'
+    } else {
+        alert("O email digitado foi: " + valorEmail)
+        elemento.Style.border = 'green solid 3px'
+    }
+}
+
+
 const validacoes = () => {
-    
+    validarNome()
+    validarEmail()
 }
 
